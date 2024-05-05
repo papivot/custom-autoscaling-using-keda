125d94bd04707d65a6c0ea800344953dad6eb2c7
Navneet Verma <47264956+papivot@users.noreply.github.com>
2024-05-05 19:34:57 -0400

    minor updates

--- SQLcommands/file.sql
+++ SQLcommands/file.sql
@@ -1,4 +1,4 @@
 CREATE TABLE user (name varchar(80), resource int(10) );
-INSERT INTO cities (name, resource) VALUES ('Mike', 1);
-INSERT INTO cities (name, resource) VALUES ('John', 2);
-DELETE from cities;
+INSERT INTO user (name, resource) VALUES ('Mike', 1);
+INSERT INTO user (name, resource) VALUES ('John', 2);
+DELETE from user;
